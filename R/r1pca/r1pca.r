dbd5346bb71256813d14f72136c17c7e4ba5cfc1
guilhermesena1 <desenabr@usc.edu>
2019-09-02 17:26:06 -0700

    Getting rid of for loops and improving speed with matrix algebra

--- R/r1pca/r1pca.r
+++ R/r1pca/r1pca.r
@@ -1,24 +1,20 @@
 #' X =  d x N matrix
 #' U = d x k matrix
-r1pca.Cr <- function(X,U,cc, xxt){
+r1pca.Cr <- function(X,U,cc){
   N <- ncol(X)
   d <- nrow(X)
 
   if(nrow(U) != d)
     stop("U Matrix rows does not match that of X")
 
-  message("Computing w")
   resid <- X - U %*% t(U) %*% X
   w <- sqrt(apply(resid^2,2,sum))
-  w <- ifelse(w <= cc, 1, cc/w)
+  if(any(w == 0))
+    stop("One of the ws = 0, error in projection")
+  w <- ifelse(w<=cc, 1, cc/w)
 
-  message("Computing Cr")
-  ans <- matrix(0,nrow=d,ncol=d)
-  for(i in 1:N){
-    ans <- ans + w[i] * xxt[[i]]
-  }
-
-  ans
+  X <- t(t(X)*sqrt(w))
+  X %*% t(X)
 }
 
 r1pca.residues <- function(X, U){
@@ -28,29 +24,36 @@ r1pca.residues <- function(X, U){
   if(nrow(U) != d)
     stop("U Matrix rows does not match that of X")
 
-  s <- sapply(1:N,function(i)
+  ans <- sapply(1:N,function(i)
     sqrt(t(X[,i])%*%X[,i] - t(X[,i])%*%U%*%t(U)%*%X[,i]))
 
-  s
+  # Debug
+  if(any(is.nan(ans)))
+    print(ans)
+
+  ans
 }
 
-r1pca <- function(X,k,niter = 100){
+r1pca <- function(X,k,verbose  = F){
   N <- ncol(X)
   U <- irlba::irlba(X, nv = k)$u
   s <- r1pca.residues(X,U)
   cc <- median(s)
 
-  message("Calculating xxt")
-  xxt <- list()
-  xxt <- lapply(1:N, function(i) X[,i] %*% t(X[,i]))
+  prev.err <- Inf
+  cur.err <- sum(r1pca.residues(X,U))
+  while(prev.err > cur.err){
+    prev.err <- cur.err
 
-  for(i in 1:niter){
-    message(paste0("Error: ",sum(r1pca.residues(X,U))))
-    Cr <- r1pca.Cr(X,U,cc, xxt)
+    if(verbose)
+      message(paste0("Error = ",cur.err))
+    Cr <- r1pca.Cr(X,U,cc)
     U <- Cr%*%U
+    U <- qr.Q(qr(U))
 
-    message("Performing gram schmidt")
-    U <- pracma::gramSchmidt(U)$Q
+    cur.err <- sum(r1pca.residues(X,U))
+    if(is.nan(cur.err) | is.na(cur.err))
+      cur.err <- Inf
   }
   list(u = U, 
        d = t(U)%*%Cr%*%U,
