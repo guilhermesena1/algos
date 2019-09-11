b5912e12a00fefc606067eff9bbd4198a935e7a1
guilhermesena1 <desenabr@usc.edu>
2019-09-11 13:21:54 -0700

    Sequence duplication, sorting frequent kmers

--- cpp/fqc/src/fqc.cpp
+++ cpp/fqc/src/fqc.cpp
@@ -21,13 +21,17 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <sys/types.h>
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
 #include <vector>
 #include <ctime>
-
 #include <string>
 #include <algorithm>
-#include <iostream>
-#include <fstream>
+#include <unordered_map>
+#include <utility>
 
 #include "smithlab_utils.hpp"
 #include "smithlab_os.hpp"
@@ -42,6 +46,9 @@ using std::reverse;
 using std::ostream;
 using std::ofstream;
 using std::cout;
+using std::unordered_map;
+using std::pair;
+using std::make_pair;
 
 // converts 64 bit integer to a sequence string by reading 3 bits at a time and
 // converting back to ACTGN
@@ -73,16 +80,16 @@ int main(int argc, const char **argv) {
   size_t kmer_size = 8;
   bool VERBOSE = false;
 
-  OptionParser opt_parse(strip_path(argv[0]), 
-                         "Quality control metrics for fastq files", 
+  OptionParser opt_parse(strip_path(argv[0]),
+                         "Quality control metrics for fastq files",
                          "<fastq-file>");
 
   // Length of k-mers to count. Grows memory exponentially so it is bound to 10
-  opt_parse.add_opt("kmer", 'k', 
+  opt_parse.add_opt("kmer", 'k',
                     "k-mer size (default = 8)", false, kmer_size);
 
-  opt_parse.add_opt("outfile", 'o', 
-                    "filename to save results (default = stdout)", 
+  opt_parse.add_opt("outfile", 'o',
+                    "filename to save results (default = stdout)",
                     false, outfile);
 
   opt_parse.add_opt("verbose", 'v', "print more run info", false, VERBOSE);
@@ -91,7 +98,7 @@ int main(int argc, const char **argv) {
   opt_parse.parse(argc, argv, leftover_args);
   if (argc == 1 || opt_parse.help_requested()) {
     cerr << opt_parse.help_message() << endl
-   << opt_parse.about_message() << endl;
+    << opt_parse.about_message() << endl;
     return EXIT_SUCCESS;
   }
   if (opt_parse.about_requested()) {
@@ -107,12 +114,12 @@ int main(int argc, const char **argv) {
     return EXIT_SUCCESS;
   }
 
-  if (kmer_size > 10){
+  if (kmer_size > 10) {
     cerr << "K-mer size should not exceed 10\n";
     cerr << opt_parse.help_message() << endl;
     return EXIT_SUCCESS;
   }
- 
+
   filename = leftover_args.front();
   /****************** END COMMAND LINE OPTIONS *****************/
 
@@ -133,7 +140,7 @@ int main(int argc, const char **argv) {
   if (mmap_data == MAP_FAILED)
     throw runtime_error("failed to mmap fastq file: " + filename);
 
-  cerr << "Fastq size: " << st.st_size /(1024*1024)<< "Mb" << endl;
+  cerr << "Fastq size: " << st.st_size /(1 << 20)<< "Mb" << endl;
   char *first = static_cast<char*>(mmap_data);
   char *last = first + st.st_size - 1;
 
@@ -146,15 +153,16 @@ int main(int argc, const char **argv) {
   const size_t num_chars = 8;  // A = 000, C = 001, T = 010, G = 011, N = 111
 
   /***********ALL****************/
-  vector<size_t> total_char(num_chars, 0);
-  vector<size_t> total_quality(num_chars, 0);
+  vector<size_t> num_bases_per_pos(num_chars, 0);  // counts for each base
+  vector<size_t> quality_per_pos(num_chars, 0);  // quality per position in read
   size_t total_bases = 0;
   size_t avg_read_length;
   double gc_content;
   double n_content;
+  double seq_duplication_level;
   size_t min_read_length = 0;
   size_t max_read_length = 0;
-  size_t exp_kmer_obs;
+  size_t exp_kmer_obs;  // Num reads * (length - k + 1) / 4^k assuming iid
 
   /*********** PER BASE METRICS ****************/
 
@@ -165,6 +173,7 @@ int main(int argc, const char **argv) {
   vector<vector<size_t>> base_quality(num_chars, vector<size_t>(kNumBases, 0));
 
   /*********** PER READ METRICS ***************/
+  unordered_map <size_t, size_t> read_freq;
 
   // Distribution of read lengths
   vector<size_t> read_length_freq(kNumBases, 0);
@@ -181,13 +190,19 @@ int main(int argc, const char **argv) {
   size_t read_pos = 0;  // which base we are at in the read
   size_t nreads = 0;  // total number of reads in fastq
   char c;  // to avoid accessing *curr multiple times
-  char buff[kNumBases];  // temporarily store the sequence for quality check
+
+  // Temporarily store the second read of the 4 lines to know the base to which
+  // quality characters are associated
+  vector<int16_t> buff(kNumBases, 0);
+
   size_t cur_kmer = 0;  // kmer hash
+  size_t read_rk_hash = 0;  // Rabin-Karp read hash
 
   if (VERBOSE) {
     cerr << "Started analysis of " << filename << "\n";
     cerr << "K-mer size: " << kmer_size << "\n";
   }
+
 
   // read character by character
   for (char *curr = first; curr < last;) {
@@ -214,30 +229,35 @@ int main(int argc, const char **argv) {
 
       // Need this to know what base the quality is associated to, therefore
       // we will store each read in memory
-      buff[read_pos] = c;
+      buff[read_pos] = base_ind;
        cur_kmer = ((cur_kmer << 3) | base_ind);
 
       // If we already read >= k bases, increment the k-mer count
-       if (read_pos >= kmer_size - 1)
-         kmer_count[cur_kmer & kmer_mask]++;
+      if (read_pos >= kmer_size - 1)
+        kmer_count[cur_kmer & kmer_mask]++;
 
+      read_rk_hash = read_rk_hash*8 + base_ind;
       read_pos++;
     }
     ++curr;  // skips \n from line 2
 
-    // *************OTHER READ NAME LINE**********************/
-    // count read length frequency
-    read_length_freq[read_pos]++;
+    // Store read statistics
+    read_length_freq[read_pos]++;  // registers the read length
+    read_freq[read_rk_hash]++;  // increments rk hash count
 
+    // Reset read statistics
+    read_pos = 0;
+    read_rk_hash = 0;
+
+    // *************OTHER READ NAME LINE**********************/
     // fast forward third line
     for (; *curr != '\n'; ++curr) {}
     ++curr;  // skips \n from line 3
 
     // *************QUALITY LINE******************************/
-    read_pos = 0;
     for (; (*curr != '\n') && (curr < last); ++curr) {
       // Gets the base from buffer, finds index, adds quality value
-      base_quality[(buff[read_pos] >> 1) & 7][read_pos] += *curr;
+      base_quality[buff[read_pos]][read_pos] += *curr;
       read_pos++;
     }
     ++curr;  // skip \n or increments from last which is not a problem
@@ -246,7 +266,7 @@ int main(int argc, const char **argv) {
   // Deallocates memory
   munmap(mmap_data, st.st_size);
 
-  if (VERBOSE) 
+  if (VERBOSE)
     cerr << "Finished reading file.\n";
 
   /**************** SUMMARIZE *********************************/
@@ -257,9 +277,9 @@ int main(int argc, const char **argv) {
   // Total frequency of each base and total number of reads
   for (size_t j = 0; j < num_chars; ++j) {
     for (size_t i = 0; i < kNumBases; ++i) {
-      total_char[j] += base_count[j][i];
+      num_bases_per_pos[j] += base_count[j][i];
       if (i == 1)
-        total_quality[j] += base_quality[j][i];
+        quality_per_pos[j] += base_quality[j][i];
       total_bases += base_count[j][i];
     }
   }
@@ -277,10 +297,10 @@ int main(int argc, const char **argv) {
 
   // Averages
   avg_read_length = total_bases / nreads;
-  gc_content = 100.0 * (total_char[1] + total_char[3])
+  gc_content = 100.0 * (num_bases_per_pos[1] + num_bases_per_pos[3])
              / static_cast<double>(total_bases);
 
-  n_content = 100.0 * total_char[7] / static_cast<double>(total_bases);
+  n_content = 100.0 * num_bases_per_pos[7] / static_cast<double>(total_bases);
 
   // Expected number of kmer observations from iid poisson
   exp_kmer_obs = static_cast<size_t>(nreads * (avg_read_length - kmer_size + 1)
@@ -300,43 +320,67 @@ int main(int argc, const char **argv) {
   os << "maximum_read_length\t" << max_read_length << "\n";
   os << "gc_frequency\t" << gc_content << "\n";
   os << "n_frequency\t" << n_content << "\n";
-  char bases[] = {'A','C','T','G','N'};
-  char inds[] = {0,1,2,3,7};
 
   // Per base statistics
-  for(size_t i = 0; i < 5; ++i){
-    os << bases[i] << "_base_quality\t ";
-    for (size_t j = 0; j < avg_read_length; ++j) {
-      double qual =  base_quality[inds[i]][j] 
-                   / static_cast<double>(base_count[inds[i]][j]);
+  vector<char> bases = {'A', 'C', 'G', 'T', 'N'};
+  for (auto base : bases) {
+    int16_t base_ind = ((base >>1) & 7);
+    os << base << "_base_quality\t ";
+    for (size_t i = 0; i < avg_read_length; ++i) {
+      double qual =  base_quality[base_ind][i]
+                   / static_cast<double>(base_count[base_ind][i]);
       qual -= ascii_to_quality;
-      os << qual << " ";
+      if (i < avg_read_length - 1)
+        os << qual << ",";
+      else
+        os << qual << "\n";
     }
 
-    os << bases[i] << "_frequency ";
-    for (size_t j = 0; j < avg_read_length; ++j) {
-      const double denom = (base_count[0][j] + base_count[1][j] +
-                            base_count[2][j] + base_count[3][j] +
-                            base_count[7][j]);
-      os << base_count[inds[i]][j]/denom << " ";
+    os << base << "_frequency ";
+    for (size_t i = 0; i < avg_read_length; ++i) {
+      const double denom = (base_count[0][i] + base_count[1][i] +
+                            base_count[2][i] + base_count[3][i] +
+                            base_count[7][i]);
+      if (i < avg_read_length - 1)
+        os << base_count[base_ind][i]/denom << ",";
+      else
+        os << base_count[base_ind][i]/denom << "\n";
     }
   }
+
+  // Sequence duplication
+  size_t dup_reads = 0;
+  for (auto v : read_freq)
+    if (v.second > 1)
+      dup_reads += v.second;
+
+  seq_duplication_level = 100.0 * dup_reads / static_cast<double>(nreads);
+  os << "seq_duplication_level\t" << seq_duplication_level << "\n";
 
   // Kmer statistics
   os << "\n";
   os << "kmer_size\t" << kmer_size << "\n";
   os << "kmer_expected_frequency\t " << exp_kmer_obs << "\n";
 
-  // All kmers
-  os << "Overrepresented k-mers (> 5 stdevs above poisson): \n";
-  for (size_t i = 0; i < kmer_mask; ++i) {
-    if (kmer_count[i] > 5 * exp_kmer_obs) {
-        os << int_to_seq(i, kmer_size) << "\t" << kmer_count[i] << "\n";
-    }
-  }
+  // Store frequent kmers
+  vector<pair<size_t, size_t>> kmer_freq_ordered;
+  for (size_t i = 0; i < kmer_mask; ++i)
+    if (kmer_count[i] > 5 * exp_kmer_obs)
+      kmer_freq_ordered.push_back(make_pair(i, kmer_count[i]));
 
-  // TODO : find adapters with significant kmer enrichment
+  // sort by frequency in decreasing order
+  sort(kmer_freq_ordered.begin(), kmer_freq_ordered.end(),
+      [](auto &a, auto &b){ return a.second > b.second; });
+
+  os << "Overrepresented k-mers (> 5 stdevs above poisson): \n";
+  for (auto v : kmer_freq_ordered)
+    os << int_to_seq(v.first, kmer_size) << "\t" << v.second  << "\n";
+
+  /************** TIME SUMMARY *********************************/
+  // TODO(gui) : find adapters with significant kmer enrichment
   if (VERBOSE)
-    cerr << "Elapsed time: " << (clock() - begin)/CLOCKS_PER_SEC << " seconds\n";
+    cerr << "Elapsed time: "
+         << (clock() - begin) / CLOCKS_PER_SEC
+         << " seconds\n";
 }
 
